5e0c09b63aac285321bc93e493a0818d2a4aae55
AB Rizvi <abrizvi@ABs-MacBook-Pro.local>
2025-03-29 16:42:59 -0400

    Get all platforms

---
+++ Controllers/PlatformController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RockServers.Data;
+
+namespace RockServers.Controllers
+{
+    [Route("api/platforms")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public PlatformController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var platforms = await _context.Platforms.ToListAsync();
+            return Ok(platforms);
+        }
+    }
+}

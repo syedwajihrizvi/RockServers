e0933377b5992aacead5faac2abacf2c0d0cc029
AB Rizvi <abrizvi@ABs-MacBook-Pro.local>
2025-05-08 10:07:05 -0400

    Added basic authentication of fields to register

--- DTO/Accounts/RegisterDto.cs
+++ DTO/Accounts/RegisterDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,19 @@ namespace RockServers.DTO.Accounts
 {
     public class RegisterDto
     {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter a name with atleast 2 Characters.")]
+        [MaxLength(255, ErrorMessage = "Please enter a name length with less than 255 Characters.")]
         public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter a name with atleast 2 Characters.")]
+        [MaxLength(255, ErrorMessage = "Please enter a name length with less than 255 Characters.")]
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter a name with atleast 2 Characters.")]
+        [MaxLength(255, ErrorMessage = "Please enter a name length with less than 255 Characters.")]
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string? Avatar { get; set; }
